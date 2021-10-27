5337228c8a3f462380c948ad1e0fed27a7c9d65f
mizuhomai <choco.hana.choco@gmail.com>
2021-10-27 18:04:27 +0900

    セクション17.200 状態をどのように管理するのか

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback  (bool? checkboxState) {
+    setState(() {
+      isChecked = checkboxState!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('今日のタスク'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      title: Text('今日のタスク',
+        style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null),
+          ),
+      trailing: TaskCheckbox(
+          checkboxState:isChecked,
+          toggleCheckboxState:checkboxCallback),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function(bool?) toggleCheckboxState;
+
+  TaskCheckbox({required this.checkboxState,
+      required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
